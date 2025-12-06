8f78959ad3f0dbcf346eaf1f8ae196c5eddc6aee
mohdvasm <mohdvasm@gmail.com>
2025-12-06 12:35:49 +0530

    Refactor code structure for improved readability and maintainability

--- demonstration/day - 3/hello.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to Python Workshop")
-
-print("Welcome to Braim School")
-
-print("Welcome to Samdani Institute")
-
-# run this code to get the output 
--- demonstration/day - 5/revision.py
+++
@@ -1,5 +0,0 @@
-""" 
-1. Use all string methods/functions
-2. Use list with some examples of indexing(3):
-3. Use list methods 
-"""
